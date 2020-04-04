bb3772c99c0bc620e20383dcdafab12330623937
hoya <hoya@localhost>
2020-04-04 19:54:58 +0900

    가상 달력찍기(1).

--- src/hoya/calendar/Prompt.java
+++ src/hoya/calendar/Prompt.java
@@ -3,26 +3,26 @@ package hoya.calendar;
 import java.util.Scanner;
 
 public class Prompt {
-	
-	private final static String PROMPT = "cal>";
-	
+
+	private final static String PROMPT = "cal> ";
+
 	public void runPrompt() {
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
 
 		int month = 0;
-		
+
 		while (true) {
 			System.out.println("달을 입력하세요.");
 			System.out.print(PROMPT);
 			month = scanner.nextInt();
-			if(month == -1) {
+			if (month == -1) {
 				break;
 			}
-			if(month > 12) {
+			if (month > 12) {
 				continue;
 			}
-			System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
+			cal.printCalendar(2020, month);
 		}
 
 		System.out.println("exit");
