5677f80482eca63b5c6442d037debd8a2c39f805
hoya <hoya@localhost>
2020-04-04 19:09:59 +0900

    로직 함수로 나누기, cal기능추가.

--- src/hoya/calendar/Calendar.java
+++ src/hoya/calendar/Calendar.java
@@ -4,25 +4,31 @@ import java.util.Scanner;
 
 public class Calendar {
 
+	private final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	public int getMaxDaysOfMonth(int month) {
+		return MAX_DAYS[month - 1];
+	}
+
+	public void sampleCalendar() {
+		System.out.println("SU MO TU WE TH FR SA");
+		System.out.println("--------------------");
+		System.out.println(" 1  2  3  4  5  6  7");
+		System.out.println(" 8  9 10 11 12 13 14");
+		System.out.println("15 16 17 18 19 20 21");
+		System.out.println("22 23 24 25 26 27 28");
+	}
+
 	public static void main(String[] args) {
-		
-//		System.out.println("SU MO TU WE TH FR SA");
-//		System.out.println("--------------------");
-//		System.out.println(" 1  2  3  4  5  6  7");
-//		System.out.println(" 8  9 10 11 12 13 14");
-//		System.out.println("15 16 17 18 19 20 21");
-//		System.out.println("22 23 24 25 26 27 28");
-		
-		
-		
+
 		Scanner scanner = new Scanner(System.in);
+		Calendar cal = new Calendar();
 		System.out.println("달을 입력하세요.");
 		int month = scanner.nextInt();
-		
-		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-		System.out.printf("%d월은 %d일까지 있습니다.", month, maxDays[month-1]);
-		
+
+		System.out.printf("%d월은 %d일까지 있습니다.", month, cal.getMaxDaysOfMonth(month));
 		scanner.close();
+
 	}
 
 }
