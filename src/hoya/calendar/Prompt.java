82260156ae66ae8a37460493f0d11eb219744848
hoya <hoya@localhost>
2020-04-05 18:29:00 +0900

    더 정교한 달력 찍기/

--- src/hoya/calendar/Prompt.java
+++ src/hoya/calendar/Prompt.java
@@ -3,30 +3,56 @@ package hoya.calendar;
 import java.util.Scanner;
 
 public class Prompt {
-
-	private final static String PROMPT = "cal> ";
+	
+	public int parseDay(String week) {
+		if (week.equals("su")) {
+			return 0;
+		} else if (week.equals("mo")) {
+			return 1;
+		} else if (week.equals("tu")) {
+			return 2;
+		} else if (week.equals("we")) {
+			return 3;
+		} else if (week.equals("th")) {
+			return 4;
+		} else if (week.equals("fr")) {
+			return 5;
+		} else if (week.equals("sa")) {
+			return 6;
+		} else {
+			return 0;
+		}
+		
+	}
 
 	public void runPrompt() {
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
 
-		int year = -1;
-		int month = -1;
+		int year = 0;
+		int month = 0;
+		int weekday = 0;
 
 		while (true) {
-			System.out.println("연도를 입력하세요.");
+			System.out.println("연도를 입력하세요. (exit: -1)");
 			System.out.print("YEAR> ");
 			year = scanner.nextInt();
+			if (year == -1) {
+				break;
+			}
 			System.out.println("달을 입력하세요.");
 			System.out.print("MONTH> ");
 			month = scanner.nextInt();
-			if (month == -1) {
-				break;
-			}
-			if (month > 12) {
+			System.out.println("첫번째 요일을 입력하세요. (SU, MO, TU, WE, TH, FR, SA).");
+			String str_weekday = scanner.next();
+			weekday = parseDay(str_weekday);
+			
+
+			if (month > 12 || month < 1) {
+				System.out.println("잘못된 입력입니다.");
 				continue;
 			}
-			cal.printCalendar(year, month);
+			cal.printCalendar(year, month, weekday);
 		}
 
 		System.out.println("exit");
